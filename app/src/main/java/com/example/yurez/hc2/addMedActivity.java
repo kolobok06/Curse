5b9a23f0fc7e37e3a1ba1eb57b673551ab51ccf7
kolobok06 <kolob060598@gmail.com>
2018-01-01 16:01:26 +0300

    01.01.18

--- app/src/main/java/com/example/yurez/hc2/addMedActivity.java
+++ app/src/main/java/com/example/yurez/hc2/addMedActivity.java
@@ -1,32 +1,93 @@
 package com.example.yurez.hc2;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.util.Calendar;
+
 public class addMedActivity extends AppCompatActivity {
+
+    EditText startDateEdit;
+    Calendar dateCal = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_med);
         setTitle(R.string.title_addMedActivity);
-//TODO: Deal with postfix. Maybe it's doesn't work cuz spinner isn't downloaded yet
- /*
-        final Spinner spinner = (Spinner) findViewById(R.id.medTypePicker);
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        startDateEdit = (EditText) findViewById(R.id.startDateEdit);
+        startDateEdit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                TextView label = (TextView) findViewById(R.id.medTypeTitle);
-                label.setText(spinner.getSelectedItem().toString());
+            public void onClick(View view) {
+                setDate(view);
             }
         });
-        */
+
+
+    }
+//region datePicker
+
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            dateCal.set(Calendar.YEAR, year);
+            dateCal.set(Calendar.MONTH, month);
+            dateCal.set(Calendar.DAY_OF_MONTH, day);
+            setInitialDate();
+
+        }
+    };
+
+    private void setInitialDate() {
+        startDateEdit.setText(
+                DateUtils.formatDateTime(this, dateCal.getTimeInMillis(),
+                        DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR /*| DateUtils.FORMAT_SHOW_TIME*/));
+    }
+
+    public void setDate(View v) {
+        new DatePickerDialog(addMedActivity.this, d,
+                dateCal.get(Calendar.YEAR),
+                dateCal.get(Calendar.MONTH),
+                dateCal.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+//endregion
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+//region adapterSetToSpinner
+/*ArrayAdapter<CharSequence> medTypeAdapter = ArrayAdapter.createFromResource(
+this, R.array.list_medType, android.R.layout.simple_spinner_item);
+medTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+final Spinner spinner = (Spinner) findViewById(R.id.medTypePicker);
+spinner.setAdapter(medTypeAdapter);
+
+spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+@Override
+public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+TextView label = (TextView) findViewById(R.id.medTypeTitle);
+label.setText(spinner.getSelectedItem().toString());
+}
+
+@Override
+public void onNothingSelected(AdapterView<?> adapterView) {
+
+}
+});*/
+//endregion
 
     }
 
